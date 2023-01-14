b1e6ada2ee2be2a32727daa3e548ed415aab91bf
ShuvoMallick01 <shuvacse01@gmail.com>
2023-01-14 19:39:08 +0600

    Update Markup HTML to Menually JS Create Element

--- js/app.js
+++ js/app.js
@@ -52,30 +52,70 @@ todoForm.addEventListener('submit', (e) => {
   const newTodo = createTodo(todoInput.value);
   todoInput.value = '';
 
-  const markup = `<div class="todo-item">
-        <div>
-          <h5 class="todo-title">${newTodo.title}</h5>
-          <p class="todo-createAt text-muted">Today at ${timeFormat(
-            new Date(newTodo.createAt)
-          )}</p>
-        </div>
+  // Create parent div
+  const parentDiv = document.createElement('div');
+  parentDiv.className = 'todo-item';
+  // Create First title
+  const firstElement = document.createElement('div');
+  const titleElement = document.createElement('h5');
+  titleElement.className = 'todo-title';
+  titleElement.textContent = newTodo.title;
+  // Create Time
+  const dateElement = document.createElement('p');
+  dateElement.className = 'todo-createAt text-muted';
+  dateElement.textContent = `Today at ${timeFormat(
+    new Date(newTodo.createAt)
+  )}`;
 
-        <div class="todo-actions d-flex align-items-center">
-          <input id="complete" class="form-check-input" type="checkbox" />
-          <i
-            id="edit"
-            class="fa-solid fa-pen-to-square text-success edit-btn"
-          ></i>
-          <i
-            id="delete"
-            class="fa-solid fa-trash text-danger delete-btn"
-          ></i>
-        </div>
-    </div>`;
+  // Create second Element
+  const secondElement = document.createElement('div');
+  secondElement.className = 'todo-actions d-flex align-items-center';
+  // Input Element
+  const inputElement = document.createElement('input');
+  inputElement.type = 'checkbox';
+  inputElement.className = 'form-check-input';
+  inputElement.id = 'complete';
+  // Edit icon Element
+  const editIconElement = document.createElement('i');
+  editIconElement.id = 'edit';
+  editIconElement.classList = 'fa-solid fa-pen-to-square text-success edit-btn';
+  // Delete icon Element
+  const deleteIconElement = document.createElement('i');
+  deleteIconElement.id = 'delete';
+  deleteIconElement.classList = 'fa-solid fa-trash text-danger delete-btn';
 
-  todoListWrapper.innerHTML += markup;
+  // append element
+  firstElement.append(titleElement, dateElement);
+  secondElement.append(inputElement, editIconElement, deleteIconElement);
+  parentDiv.append(firstElement, secondElement);
 
-  //   Delete Todo
+  todoListWrapper.append(parentDiv);
+
+  // html markup
+  // const markup = `<div class="todo-item">
+  //       <div>
+  //         <h5 class="todo-title">${newTodo.title}</h5>
+  //         <p class="todo-createAt text-muted">Today at ${timeFormat(
+  //           new Date(newTodo.createAt)
+  //         )}</p>
+  //       </div>
+
+  //       <div class="todo-actions d-flex align-items-center">
+  //         <input id="complete" class="form-check-input" type="checkbox" />
+  //         <i
+  //           id="edit"
+  //           class="fa-solid fa-pen-to-square text-success edit-btn"
+  //         ></i>
+  //         <i
+  //           id="delete"
+  //           class="fa-solid fa-trash text-danger delete-btn"
+  //         ></i>
+  //       </div>
+  //   </div>`;
+
+  // todoListWrapper.innerHTML += markup;
+
+  // Delete Todo
   const deleteBtnList = document.querySelectorAll('#delete');
 
   deleteBtnList.forEach((element) => {
@@ -85,7 +125,7 @@ todoForm.addEventListener('submit', (e) => {
     });
   });
 
-  //Complete Todo
+  // Complete Todo
   const completeCheckList = document.querySelectorAll('#complete');
 
   completeCheckList.forEach((element) => {
@@ -105,8 +145,10 @@ todoForm.addEventListener('submit', (e) => {
   // console.log(editTodo);
   editTodo.forEach((element) => {
     element.addEventListener('click', (e) => {
-      // todoInput.value = editTodo.innerText;
-      console.log(editTodo.innertext);
+      // createTodo().title = editTodo.innerText;
+      todoInput.value = createTodo(e.innerHTML);
+      // console.log(editTodo.innertext);
+      // console.log(e);
     });
   });
 });
